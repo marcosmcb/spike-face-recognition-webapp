ff5a42fe25851db873f607c6d3437e99bb4ff131
Marcos Cavalcante <marcos.bcc2011@gmail.com>
2020-03-02 10:57:45 +0000

    refactor(emotion detection): refactor methods related to face detection to their own module

--- face_emotion_detection.py
+++ face_emotion_detection.py
@@ -1,63 +1,9 @@
 import face_recognition
 import cv2
-import numpy as np
-from keras.models import load_model
-from keras.preprocessing import image
-from keras.preprocessing.image import img_to_array
-from keras.applications import imagenet_utils
-import tensorflow as tf
 import time
 from utils.helper_functions import get_frames, display_results
 from utils.face_recognition import get_faces, recognise_faces
-from keras.models import model_from_json
-
-#-----------------------------
-#face expression recognizer initialization
-face_cascade = cv2.CascadeClassifier('./utils/models/haarcascade_frontalface_default.xml')
-emotions = ('angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral')
-#-----------------------------
-model = None
-graph = None
-
-    
-def load_model():
-    # load the pre-trained Keras model (here we are using a model
-    # pre-trained on ImageNet and provided by Keras, but you can
-    # substitute in your own networks just as easily)
-    global model
-    model = model_from_json(open("./utils/models/facial_expression_model_structure.json", "r").read())      
-    model.load_weights('./utils/models/facial_expression_model_weights.h5')  
-    global graph
-    # save graph after loading ResNet50 weights
-    graph = tf.get_default_graph()
-
-
-
-def get_emotions(gray, img):
-    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
-    for (x,y,w,h) in faces:
-        if w > 130: #trick: ignore small faces
-            
-            detected_face = img[int(y):int(y+h), int(x):int(x+w)] #crop detected face
-            detected_face = cv2.cvtColor(detected_face, cv2.COLOR_BGR2GRAY) #transform to gray scale
-            detected_face = cv2.resize(detected_face, (48, 48)) #resize to 48x48
-            
-            img_pixels = img_to_array(detected_face)
-            img_pixels = np.expand_dims(img_pixels, axis = 0)
-            
-            img_pixels /= 255 #pixels are in scale of [0, 255]. normalize all pixels in scale of [0, 1]
-            
-            #------------------------------
-            with graph.as_default():
-                predictions = model.predict(img_pixels)
-            # predictions = get_model().predict(img_pixels) #store probabilities of 7 expressions
-            max_index = np.argmax(predictions[0])
-            
-            
-            emotions = ['angry', 'disgust', 'fear', 'happy', 'sad', 'surprise', 'neutral']
-            predicted_emotion = emotions[max_index]
-            return predicted_emotion
-
+from utils.emotion_detection import load_model, detect_emotions
 
 
 def detect(socketio):
@@ -85,7 +31,7 @@ def detect(socketio):
         if process_this_frame:
             face_locations, face_encodings = get_faces(rgb_small_frame, frame)
             face_names = recognise_faces(face_encodings, known_face_encodings, known_face_names)
-            emotion = get_emotions(gray_frame, frame)
+            emotion = detect_emotions(gray_frame, frame)
             emotion = emotion if emotion != None else " "
         process_this_frame = not process_this_frame
 
